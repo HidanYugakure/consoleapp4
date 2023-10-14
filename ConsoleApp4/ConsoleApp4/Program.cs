b571d06db8bc9ce17d70b6824e5ae11ba3cd527e
HidanYugakure <fidants@code.edu.az>
2023-10-15 00:01:48 +0400

    Task 4 console 4

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,55 @@
+﻿
+#region //task1
+//int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, };
+//int outCome = 0;
+//foreach (int number in numbers)
+//{
+//    outCome += number;
+//}
+//Console.WriteLine(outCome);
+#endregion
+
+#region //task2
+//int[] numbers = { 1, 3, 15, 28, 90 };
+//int result = 0;
+
+//foreach (int number in numbers)
+//{
+//    if (number >= 10)
+//    {
+//        result++;
+//    }
+
+//}
+//Console.WriteLine(result);
+#endregion
+
+#region //task 3
+//int[] numbers = { 3, 5, 9, 15, 70 };
+//int result = 0;
+//int counter = 0;
+
+//foreach (int number in numbers)
+//{
+//    result += number;
+//}
+//Console.WriteLine(result / numbers.Length);
+
+#endregion
+
+#region /task4
+
+//string[] aylar = { "yanvar", "fevral", "mart", "aprel", "may", "iyun", "iyul", "avqust", "sentyabr", "oktyabr", "noyabr", "dekabr" };
+
+//foreach (string ay in aylar)
+//{
+//    if (ay == "yanvar" || ay == "fevral" || ay == "dekabr")
+//    {
+//        Console.WriteLine($"{ay} qis ayidir");
+//    }
+
+//}
+#endregion
+
+
+
